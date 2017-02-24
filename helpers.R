d6bd03d4785d9e4b61f757fa43db3a8ae83a3e10
sharan-naribole <sn23@rice.edu>
2017-02-23 22:27:24 -0600

    high applicants and data scientist job comparison done

--- helpers.R
+++ helpers.R
@@ -193,7 +193,8 @@ get_theme <- function() {
           legend.position = "right",
           legend.text = element_text(size = rel(1.5)),
           legend.title = element_text(size=rel(1.5)),
-          axis.text = element_text(size=rel(1.5))) 
+          axis.text.y = element_text(size=rel(1.5),face="bold"),
+          axis.text.x = element_text(size=rel(1.25),face="bold")) 
   )
 }
 
